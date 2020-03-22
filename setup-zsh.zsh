3ac16f34c307bd4effb56fdabad2d465d70b3daa
Patrick McDonald <764290+WhatsThatItsPat@users.noreply.github.com>
2020-03-22 14:44:27 -0400

    Add conditionals in setup-zsh.zsh

--- setup-zsh.zsh
+++ setup-zsh.zsh
@@ -2,6 +2,17 @@
 
 echo "\n~~~ Starting ZSH Setup ~~~\n"
 
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells'
+# Installation unnecessary; it's already in Brewfile.
 
-chsh -s /usr/local/bin/zsh
+# https://stackoverflow.com/a/4749368/1341838
+if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
+  echo '/usr/local/bin/zsh already exists in /etc/shells'
+else
+  echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells'
+fi
+
+if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
+  echo '$SHELL is already /usr/local/bin/zsh'
+else
+  chsh -s /usr/local/bin/zsh
+fi
