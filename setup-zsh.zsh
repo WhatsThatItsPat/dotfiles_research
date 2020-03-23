b159e685eca8034154b6309954a67c0ba138014b
Patrick McDonald <764290+WhatsThatItsPat@users.noreply.github.com>
2020-03-23 16:30:40 -0400

    Prevent superfluous commands with conditionals

--- setup-zsh.zsh
+++ setup-zsh.zsh
@@ -4,15 +4,30 @@ echo "\n~~~ Starting ZSH Setup ~~~\n"
 
 # Installation unnecessary; it's already in Brewfile.
 
+
 # https://stackoverflow.com/a/4749368/1341838
 if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
   echo '/usr/local/bin/zsh already exists in /etc/shells'
 else
-  echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells'
+  echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 fi
+
 
 if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
   echo '$SHELL is already /usr/local/bin/zsh'
 else
   chsh -s /usr/local/bin/zsh
 fi
+
+
+# if command ls -la /private/var/select/sh | grep -q /bin/zsh; then
+if sh --version | grep -q zsh; then
+  echo '/private/var/select/sh already linked to /bin/zsh'
+else
+  # Looked cute, might delete later, idk
+  sudo ln -sfv /bin/zsh /private/var/select/sh
+  
+  # I'd like for this to work instead.
+  # sudo ln -sfv /usr/local/bin/zsh /private/var/select/sh
+fi
+
