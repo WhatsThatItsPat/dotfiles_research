6fbd1d0ac608d26af4ab791b90217d5428620263
Patrick McDonald <764290+WhatsThatItsPat@users.noreply.github.com>
2020-03-25 14:28:33 -0400

    Install Global NPM Packages
    Firebase, Angular, Ionic, TypeScript, and json / http servers.

--- setup-node.zsh
+++ setup-node.zsh
@@ -12,3 +12,13 @@ else
   n latest # or lts
 fi
 
+
+# Install NPM Packages
+npm install --global firebase-tools
+npm install --global @angular/cli
+npm install --global @ionic/cli
+npm install --global typescript
+npm install --global json-server
+npm install --global http-server
+
+
