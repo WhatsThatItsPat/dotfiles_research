6d8feeedbb8190b5dded6e32aed08d7ef75efca1
Patrick McDonald <764290+WhatsThatItsPat@users.noreply.github.com>
2020-03-25 14:27:35 -0400

    Adjust comments

--- setup-node.zsh
+++ setup-node.zsh
@@ -2,13 +2,13 @@
 
 echo "\n~~~ Starting Node Setup ~~~\n"
 
-# Installation unnecessary; it's already in Brewfile.
-# See zshrc for N_PREFIX and N_PREFIX/bin addition to PATH.
+# Node versions are managed with n, which is already in Brewfile.
+# See zshrc for N_PREFIX and N_PREFIX/bin addition to $PATH.
 
 if exists node; then
-  echo "Node Version: $(node --version)"
-  echo "NPM Version: $(npm --version)"
+  echo "Node ($(node --version)) & NPM ($(npm --version)) already installed"
 else
+  echo "Installing Node and NPM..."
   n latest # or lts
 fi
 
