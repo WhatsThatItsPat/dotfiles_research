6365bf80fd919857c8a79e52982674c507a2931b
Patrick McDonald <764290+WhatsThatItsPat@users.noreply.github.com>
2020-05-27 16:57:05 -0400

    Set dock to magnify and autohide

--- setup_macos.zsh
+++ setup_macos.zsh
@@ -10,3 +10,18 @@ defaults write com.apple.finder ShowPathbar -bool true
 # Finder > Preferences > General > New Finder windows show:
 defaults write com.apple.finder NewWindowTarget -string 'PfLo'
 defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME/.dotfiles"
+
+
+# System Preferences > Dock
+defaults write com.apple.dock magnification -bool true
+defaults write com.apple.dock tilesize -float 50
+defaults write com.apple.dock largesize -float 70
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-time-modifier -float 0.25
+defaults write com.apple.dock autohide-delay -float 0.1
+
+# Finish macOS Setup
+killall Finder
+killall Dock
+echo "Done with macOS setup."
+
